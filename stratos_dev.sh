d1e5f658cbee89be5fd06afbb2b7c0078e523821
snowch <chsnow123@gmail.com>
2014-04-09 22:01:32 +0100

    add more info for users

--- stratos_dev.sh
+++ stratos_dev.sh
@@ -91,6 +91,9 @@ Where:
        ----------------------------------------------------------------
        IMPORTANT: 
        The first time you run this script must be with the command '-f'
+
+       You must also configure the iaas.conf file in the /home/vagrant
+       folder with the details of your IaaS.
        ----------------------------------------------------------------
 
     -f perform a complete setup of the stratos runtime environment
