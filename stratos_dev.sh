45be397a70b91995b1a77062134c45ec7f059ce1
Chris Snow <chsnow123@gmail.com>
2014-04-17 17:57:50 +0100

    Update stratos_dev.sh

--- stratos_dev.sh
+++ stratos_dev.sh
@@ -71,7 +71,7 @@ HAWTBUF_FILE="hawtbuf-1.2.jar"
 # You should not need to change anything below this line
 ########################################################
 
-if egrep -qv "Ubuntu (12.04|13.04)" /etc/issue; then
+if ! egrep -q "Ubuntu (12.04|13.04)" /etc/issue; then
   echo "WARNING: This script has only been tested on Ubuntu 12.04 and 13.04"
   read -p "Press [Enter] key to continue (CTRL-C to quit)..."  
   clear
