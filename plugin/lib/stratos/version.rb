08f43bff517fcf2104e12e4b2d255499dddc4f9d
Chris Snow <chsnow123@gmail.com>
2014-06-26 20:56:13 +0000

    removed plugin folder

--- plugin/lib/stratos/version.rb
+++
@@ -1,3 +0,0 @@
-module Stratos
-  VERSION = "0.0.1"
-end
--- plugin/ruby_plugin_env.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
-curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
-rvm install 2.1.2
-cd /vagrant/plugin/stratos/
-bundle install
-bundle exec rake build
