040ae1060d559090185cd39abb70bd2f3a61e73c
Chris Snow <Chris.Snow@monitise.com>
2014-07-19 19:05:01 +0100

    add autoscaler configuration

---
+++ stratos-docker/stop_and_remove_all_containers.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo docker stop $(sudo docker ps -a -q) 
+sudo docker rm $(sudo docker ps -a -q)
--- stratos-docker/stratos-single-jvm/docker-build.sh
+++ stratos-docker/stratos-single-jvm/docker-build.sh
@@ -4,6 +4,8 @@
 
 cp /home/vagrant/stratos-source/products/stratos/modules/distribution/target/apache-stratos-4.0.0-SNAPSHOT.zip files/apache-stratos.zip
 
+wget -N -q -P -nc files/ http://archive.apache.org/dist/activemq/5.9.1/apache-activemq-5.9.1-bin.tar.gz
+
 cp -rf /home/vagrant/stratos-source/tools/stratos-installer/ files/
 
 sudo docker build -t=apachestratos/stratos-single-jvm .
