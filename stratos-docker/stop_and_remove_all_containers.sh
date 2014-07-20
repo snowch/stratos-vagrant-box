1587486d87e0c84c008936bdc928777aa822a8f3
Chris Snow <Chris.Snow@monitise.com>
2014-07-20 08:20:33 +0100

    add initial SM setup

--- stratos-docker/stop_and_remove_all_containers.sh
+++ stratos-docker/stop_and_remove_all_containers.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 sudo docker stop $(sudo docker ps -a -q) 
+
 sudo docker rm $(sudo docker ps -a -q)
--- stratos-docker/stratos-single-jvm/docker-build.sh
+++ stratos-docker/stratos-single-jvm/docker-build.sh
@@ -4,7 +4,9 @@
 
 cp /home/vagrant/stratos-source/products/stratos/modules/distribution/target/apache-stratos-4.0.0-SNAPSHOT.zip files/apache-stratos.zip
 
-wget -N -q -P -nc files/ http://archive.apache.org/dist/activemq/5.9.1/apache-activemq-5.9.1-bin.tar.gz
+wget -N -q -P files/ http://archive.apache.org/dist/activemq/5.9.1/apache-activemq-5.9.1-bin.tar.gz
+
+wget -N -q -P files/ http://repo1.maven.org/maven2/mysql/mysql-connector-java/5.1.29/mysql-connector-java-5.1.29.jar
 
 cp -rf /home/vagrant/stratos-source/tools/stratos-installer/ files/
 
