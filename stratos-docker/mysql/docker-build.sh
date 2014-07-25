db6bb85c6cc9d453677b33e3aaa2e4edbf7032e3
Chris Snow <Chris.Snow@monitise.com>
2014-07-25 07:06:53 +0100

    refactor script

--- stratos-docker/mysql/docker-build.sh
+++ stratos-docker/mysql/docker-build.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-if ! which docker.io; then
-   sudo apt-get update
-   sudo apt-get install -y docker.io
-fi
+### copy stratos.mysql
 
-sudo docker.io build -t=apachestratos/mysql .
-sudo docker.io push apachestratos/mysql
+# mysql container uses--init-file option which does not like comments 
+# and likes each command on its own line
+
+cp -f /home/vagrant/stratos-source/tools/stratos-installer/resources/mysql.sql files/mysql.tmp.0
+
+# strip singleline comments
+grep -v '^--.*$' files/mysql.tmp.0 > files/mysql.tmp.1
+
+# strip multiline comments
+perl -0777 -pe 's{/\*.*?\*/}{}gs' files/mysql.tmp.1 > files/mysql.sql
+
+# remove newlines
+sed -i ':a;N;$!ba;s/\n/ /g' files/mysql.sql
+
+# replace ; with ;\n
+sed -i 's/;/;\n/g' files/mysql.sql
+
+rm files/mysql.tmp.*
+
+### build docker
+
+sudo docker build -t=apachestratos/mysql .
+#sudo docker push apachestratos/mysql
